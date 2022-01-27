de1e426c040ff5d9d6f1f4d67d7fe608899b80b8
Nathaniel Pather <nathaniel_pather@hotmail.com>
2022-01-27 19:12:27 +1000

    Add router

---
+++ api/routes/CalculatorRoutes.js
@@ -0,0 +1,8 @@
+'use strict';
+module.exports = function(app){
+    var calculator = require('../controllers/CalculatorController');
+}
+
+// Calculator Routes
+app.route('calculate')
+    .get(calculator.calculate);
